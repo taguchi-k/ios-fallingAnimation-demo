86dd41a1a1e31c2ea599cc379365a72a56930b40
taguchi-k <k.taguchi@st-ventures.jp>
2017-04-11 13:28:11 +0900

    Initial Commit

---
+++ ios-fallingAnimation-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-fallingAnimation-demo
+//
+//  Created by Kentaro on 2017/04/11.
+//  Copyright © 2017年 Kentao Taguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
